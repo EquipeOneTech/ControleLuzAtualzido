bf0f5d323550ff525e7047e4489a88f46a5e4425
Felipe Cardoso Coelho <simonedutra484@gmail.com>
2020-06-13 17:23:04 -0300

    customizando graficos

--- app/src/main/java/com/equipeonetech/apptest/GraphicScreen.java
+++ app/src/main/java/com/equipeonetech/apptest/GraphicScreen.java
@@ -1,24 +1,22 @@
 package com.equipeonetech.apptest;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.BarGraphSeries;
 
 
 public class GraphicScreen extends AppCompatActivity {
-    EditText firstNum_1, secondNum_1;
-    EditText firstNum_2, secondNum_2;
-    EditText firstNum_3, secondNum_3;
-    EditText firstNum_4, secondNum_4;
+
     Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,36 +30,55 @@ public class GraphicScreen extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstInput_1, secondInput_1;
-                String firstInput_2, secondInput_2;
-                String firstInput_3, secondInput_3;
-                String firstInput_4, secondInput_4;
-                //  1 and 5
-//                firstInput_1 = firstNum_1.getText().toString();
-//                secondInput_1 = secondNum_1.getText().toString();
-//                firstInput_2 = firstNum_2.getText().toString();
-//                secondInput_2 = secondNum_2.getText().toString();
-//                firstInput_3 = firstNum_3.getText().toString();
-//                secondInput_3 = secondNum_3.getText().toString();
-//                firstInput_4 = firstNum_4.getText().toString();
-//                secondInput_4 = secondNum_4.getText().toString();
                 try {
                     BarGraphSeries< DataPoint > series = new BarGraphSeries < > (new DataPoint[] {
-                            new DataPoint(0,5),
-                            new DataPoint(Integer.valueOf(1), Integer.valueOf(2)),
-                            new DataPoint(Integer.valueOf(3), Integer.valueOf(4)),
-                            new DataPoint(Integer.valueOf(5), Integer.valueOf(6)),
-                            new DataPoint(Integer.valueOf(7), Integer.valueOf(8))
+                            new DataPoint(0,0),
+                            new DataPoint(1, 5),
+                            new DataPoint(2, 3),
+                            new DataPoint(3, 2),
+                            new DataPoint(4, 10),
+                            new DataPoint(5, 8),
+                            new DataPoint(6, 35),
+
                     });
-//                    series.setTitle("test1");
-////                    series.setDataWidth(20.0);
-////                    series.setAnimated(true);
-////                    series.setSpacing(10);
-////                    series.setColor(R.color.colorGreen);
+
                     graph.addSeries(series);
 
+                    
+
+                    // activate horizontal zooming and scrolling
+                    graph.getViewport().setScalable(true);
+
+                    // activate horizontal scrolling
+                    graph.getViewport().setScrollable(true);
+
+                    // activate horizontal and vertical zooming and scrolling
+                    graph.getViewport().setScalableY(true);
+
+                    // activate vertical scrolling
+                    graph.getViewport().setScrollableY(true);
+
+                    double maxX = 7.0;
+                    graph.getViewport().setXAxisBoundsManual(true);
+                    graph.getViewport().setMaxX(maxX);
+                    series.setSpacing(50);
+                    series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+                        @Override
+                        public int get(DataPoint data) {
+                            return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6),100);
+                        }
+                    });
+
+                    //series.setDrawValuesOnTop(true);
+                    series.setValuesOnTopColor(Color.RED);
+//                    series.setDataWidth(20.0);
+//                    series.setAnimated(true);
+//                    series.setSpacing(10);
+//                    series.setColor(R.color.colorGreen);
+
+
 //                    StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-//                    staticLabelsFormatter.setHorizontalLabels(new String[]{"Jan","Fev","MAR","ABR"});
+//                    staticLabelsFormatter.setHorizontalLabels(new String[]{"Jan","Fev","Mar","Abr"});
 
                 } catch (IllegalArgumentException e) {
                     Toast.makeText(GraphicScreen.this, e.getMessage(), Toast.LENGTH_LONG).show();
