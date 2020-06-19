8ac8d5a0487720ca1b4dff0eb27ae2aa41a09b39
Felipe Cardoso Coelho <simonedutra484@gmail.com>
2020-06-18 22:57:09 -0300

    added new graph lib anychart, columnGraph

--- app/src/main/java/com/equipeonetech/apptest/GraphicScreen.java
+++ app/src/main/java/com/equipeonetech/apptest/GraphicScreen.java
@@ -1,106 +1,84 @@
 package com.equipeonetech.apptest;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.jjoe64.graphview.DefaultLabelFormatter;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.BarGraphSeries;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GraphicScreen extends AppCompatActivity {
-
-    Button button;
+    private AnyChartView anyChartView;
+    private Button btVoltar;
+    private Context context = this;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphic_screen);
-        final GraphView graph = (GraphView) findViewById(R.id.graph);
-        /**Initi the components*/
+
+        /**Initi components*/
         initComponents();
-        graph.setVisibility(View.VISIBLE);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    BarGraphSeries< DataPoint > series = new BarGraphSeries < > (new DataPoint[] {
-                            new DataPoint(0,0),
-                            new DataPoint(1, 5),
-                            new DataPoint(2, 3),
-                            new DataPoint(3, 2),
-                            new DataPoint(4, 10),
-                            new DataPoint(5, 8),
-                            new DataPoint(6, 35),
+        /**Set and initi value in the graph*/
+        setColumnGraph();
+   }
 
-                    });
+    private void setColumnGraph() {
+        Cartesian cartesian = AnyChart.column();
 
-                    graph.addSeries(series);
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("Jan", 289.2));
+        data.add(new ValueDataEntry("Fev", 285.11));
+        data.add(new ValueDataEntry("Mar", 280.44));
+        data.add(new ValueDataEntry("Abr", 325.14));
+        data.add(new ValueDataEntry("Mai", 247.20));
+        data.add(new ValueDataEntry("Jun", 246.88));
 
-                    
+        Column column = cartesian.column(data);
 
-                    // activate horizontal zooming and scrolling
-                    graph.getViewport().setScalable(true);
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("${%Value}{groupsSeparator: }");
 
-                    // activate horizontal scrolling
-                    graph.getViewport().setScrollable(true);
-
-                    // activate horizontal and vertical zooming and scrolling
-                    graph.getViewport().setScalableY(true);
-
-                    // activate vertical scrolling
-                    graph.getViewport().setScrollableY(true);
-
-                    double maxX = 7.0;
-                    graph.getViewport().setXAxisBoundsManual(true);
-                    graph.getViewport().setMaxX(maxX);
-                    series.setSpacing(50);
-                    series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-                        @Override
-                        public int get(DataPoint data) {
-                            return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6),100);
-                        }
-                    });
-
-                    //series.setDrawValuesOnTop(true);
-                    series.setValuesOnTopColor(Color.RED);
-//                    series.setDataWidth(20.0);
-//                    series.setAnimated(true);
-//                    series.setSpacing(10);
-//                    series.setColor(R.color.colorGreen);
+        cartesian.animation(true);
+        cartesian.title("Faturas nos últimos 6 meses.");
 
 
-//                    StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-//                    staticLabelsFormatter.setHorizontalLabels(new String[]{"Jan","Fev","Mar","Abr"});
+        cartesian.yScale().minimum(0d);
 
-                } catch (IllegalArgumentException e) {
-                    Toast.makeText(GraphicScreen.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+        cartesian.yAxis(0).labels().format("${%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
 
 
 
+//        cartesian.xAxis(0).title("Product");
+//        cartesian.yAxis(0).title("Revenue");
+
+        anyChartView.setChart(cartesian);
 
     }
 
     private void initComponents() {
-//        firstNum_1 = findViewById(R.id.firstNum_1);
-//        secondNum_1 = findViewById(R.id.secondNum_1);
-//        firstNum_2 = findViewById(R.id.firstNum_2);
-//        secondNum_2 = findViewById(R.id.secondNum_2);
-//        firstNum_3 = findViewById(R.id.firstNum_3);
-//        secondNum_3 = findViewById(R.id.secondNum_3);
-//        firstNum_4 = findViewById(R.id.firstNum_4);
-//        secondNum_4 = findViewById(R.id.secondNum_4);
-
-        button = findViewById(R.id.addButton);
+        anyChartView = findViewById(R.id.myColumnGraph);
+        btVoltar = findViewById(R.id.btVoltar);
     }
 }
